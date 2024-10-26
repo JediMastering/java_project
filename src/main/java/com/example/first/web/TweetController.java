a01cbf80f712222997ff581cbd3364b2238264a8
Alexandre <jedimasteringale@gmail.com>
2024-10-26 20:05:23 -0300

    Valida se é admin ou dono do tweet e apaga tweet

--- src/main/java/com/example/first/web/TweetController.java
+++ src/main/java/com/example/first/web/TweetController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.example.first.dto.CreateTweetDto;
+import com.example.first.entity.Role;
 import com.example.first.entity.Tweet;
 import com.example.first.repository.TweetRepository;
 import com.example.first.repository.UserRepository;
@@ -27,6 +28,23 @@ public class TweetController {
         this.userRepository = userRepository;
     }
 
+    // @GetMapping("/feed")
+    // public ResponseEntity<FeedDto> feed(@RequestParam(value = "page", defaultValue = "0") int page,
+    //                                     @RequestParam(value = "pageSize", defaultValue = "10") int pageSize) {
+
+    //     var tweets = tweetRepository.findAll(
+    //             PageRequest.of(page, pageSize, Sort.Direction.DESC, "creationTimestamp"))
+    //             .map(tweet ->
+    //                     new FeedItemDto(
+    //                             tweet.getTweetId(),
+    //                             tweet.getContent(),
+    //                             tweet.getUser().getUsername())
+    //             );
+
+    //     return ResponseEntity.ok(new FeedDto(
+    //             tweets.getContent(), page, pageSize, tweets.getTotalPages(), tweets.getTotalElements()));
+    // }
+
     @PostMapping("/tweets")
     public ResponseEntity<Void> createTweet(@RequestBody CreateTweetDto dto,
                                             JwtAuthenticationToken token) {
@@ -37,6 +55,27 @@ public class TweetController {
         tweet.setContent(dto.content());
 
         tweetRepository.save(tweet);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/tweets/{id}")
+    public ResponseEntity<Void> deleteTweet(@PathVariable("id") Long tweetId,
+                                            JwtAuthenticationToken token) {
+        var user = userRepository.findById(UUID.fromString(token.getName()));
+        var tweet = tweetRepository.findById(tweetId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        var isAdmin = user.get().getRoles()
+                .stream()
+                .anyMatch(role -> role.getName().equalsIgnoreCase(Role.Values.ADMIN.name()));
+
+        if (isAdmin || tweet.getUser().getUserId().equals(UUID.fromString(token.getName()))) {
+            tweetRepository.deleteById(tweetId);
+
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
 
         return ResponseEntity.ok().build();
     }
