be4d53f4af2dde5e19368993ac48c7c2a4c4b69f
Alexandre <jedimasteringale@gmail.com>
2024-10-26 19:57:58 -0300

    Obtendo usuário e criando tweet

---
+++ src/main/java/com/example/first/web/TweetController.java
@@ -0,0 +1,43 @@
+package com.example.first.web;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.example.first.dto.CreateTweetDto;
+import com.example.first.entity.Tweet;
+import com.example.first.repository.TweetRepository;
+import com.example.first.repository.UserRepository;
+
+import java.util.UUID;
+
+@RestController
+public class TweetController {
+
+    private final TweetRepository tweetRepository;
+    private final UserRepository userRepository;
+
+    public TweetController(TweetRepository tweetRepository,
+                           UserRepository userRepository) {
+        this.tweetRepository = tweetRepository;
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/tweets")
+    public ResponseEntity<Void> createTweet(@RequestBody CreateTweetDto dto,
+                                            JwtAuthenticationToken token) {
+        var user = userRepository.findById(UUID.fromString(token.getName()));
+
+        var tweet = new Tweet();
+        tweet.setUser(user.get());
+        tweet.setContent(dto.content());
+
+        tweetRepository.save(tweet);
+
+        return ResponseEntity.ok().build();
+    }
+}
